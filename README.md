7a9cfe0064b044fe89b7ad20fd21dd4ce33f04fe
robel145 <129504103+robel145@users.noreply.github.com>
2023-04-10 17:52:57 +0200

    Created window

--- README.md
+++
@@ -1,2 +0,0 @@
-# projektkurs-spel
-#test
--- src/main.c
+++ src/main.c
@@ -1,1 +1,37 @@
+
 #include <stdio.h>
+
+int main(int argv, char** args)
+{
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Window *window = SDL_CreateWindow("Yo SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+
+    int isRunning = 1;
+    SDL_Event event;
+
+    while (isRunning)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+            case SDL_QUIT:
+                isRunning = 0;
+                break;
+            }
+        }
+
+        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
