98de53bafe20986ee4a9fa6bf05bcb813844ae2c
robel145 <129504103+robel145@users.noreply.github.com>
2023-04-12 20:20:02 +0200

    Created backgroundImage

--- src/main.c
+++ src/main.c
@@ -1,12 +1,25 @@
 
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 
 int main(int argv, char** args)
 {
     
     SDL_Window *window = SDL_CreateWindow("Zombies COD", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+    SDL_Surface* backgroundImage = IMG_Load("C:/Users/46729/Documents/GitHub/projektkurs-spel/resources/28256.jpg");
+    if(!backgroundImage){
+        printf("Error! BackgroundImage failed\n", IMG_GetError());
+        return 1;
+    }
+    SDL_Texture* backgroundTexture = SDL_CreateTextureFromSurface(renderer, backgroundImage);
+    if (!backgroundTexture){
+        printf("Failed to create background texture: %s\n", SDL_GetError());
+        SDL_FreeSurface(backgroundImage);
+        return 1;
+    }
+
 
     int isRunning = 1;
     SDL_Event event;
@@ -25,10 +38,11 @@ int main(int argv, char** args)
 
         SDL_RenderClear(renderer);
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-
+        SDL_RenderCopy(renderer, backgroundTexture, NULL, NULL);
         SDL_RenderPresent(renderer);
     }
 
+    SDL_DestroyTexture(backgroundTexture);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
