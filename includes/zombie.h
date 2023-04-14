00ebd57f051f7cdf4a5c80569a638d75ab51b25d
Libanl <129501428+Libanl@users.noreply.github.com>
2023-04-14 03:55:15 +0200

    Zombies
    Added: zombies, basic "random movement", updated makefile to include zombie.c and zombie header file, added zombie sprite to resource folder.

---
+++ includes/zombie.h
@@ -0,0 +1,13 @@
+#ifndef ZOMBIE_H
+#define ZOMBIE_H
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+void spawn_zombies(SDL_Rect *zombieRects, int numZombies, SDL_Surface *zombieImage, SDL_Texture *zombieTexture, int screenWidth, int screenHeight);
+void move_zombies(SDL_Rect *zombieRects, int numZombies, int movementSpeed, int screenWidth);
+void render_zombies(SDL_Renderer *renderer, SDL_Texture *zombieTexture, SDL_Rect *zombieRects, int numZombies);
+void moveZombiesRandomly(SDL_Rect* zombieRect, int numZombies, int screenWidth, int screenHeight);
+void updateZombies(SDL_Rect * zombieRect, int size);
+
+#endif
