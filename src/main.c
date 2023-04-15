1e0f62f9d32c3e808cc6746014318edc03b7f7c5
Libanl <129501428+Libanl@users.noreply.github.com>
2023-04-15 14:52:13 +0200

    fixat makefile
    tagit bort updated upstream?

--- src/main.c
+++ src/main.c
@@ -4,23 +4,16 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include "../includes/zombie.h" // include the zombies header file
-<<<<<<< Updated upstream
-=======
 #include "../includes/spelare.h"
 #define WINDOW_WIDTH 300
 #define WINDOW_HEIGHT 250
->>>>>>> Stashed changes
 
 
 int main(int argv, char **args)
 {
     SDL_Window *window = SDL_CreateWindow("Zombies COD", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
-<<<<<<< Updated upstream
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
-=======
     SDL_Renderer *pRenderer = SDL_CreateRenderer(window, -1, 0);
     
->>>>>>> Stashed changes
 
     SDL_Surface *backgroundImage = IMG_Load("resources/28256.jpg");
     if (!backgroundImage)
@@ -29,11 +22,7 @@ int main(int argv, char **args)
         return 1;
     }
 
-<<<<<<< Updated upstream
-    SDL_Texture *backgroundTexture = SDL_CreateTextureFromSurface(renderer, backgroundImage);
-=======
     SDL_Texture *backgroundTexture = SDL_CreateTextureFromSurface(pRenderer, backgroundImage);
->>>>>>> Stashed changes
     if (!backgroundTexture)
     {
         printf("Failed to create background texture: %s\n", SDL_GetError());
@@ -50,11 +39,7 @@ int main(int argv, char **args)
         return 1;
     }
 
-<<<<<<< Updated upstream
-    SDL_Texture *zombieTexture = SDL_CreateTextureFromSurface(renderer, zombieImage);
-=======
     SDL_Texture *zombieTexture = SDL_CreateTextureFromSurface(pRenderer, zombieImage);
->>>>>>> Stashed changes
     if (!zombieTexture)
     {
         printf("Failed to create zombie texture: %s\n", SDL_GetError());
@@ -63,8 +48,6 @@ int main(int argv, char **args)
         SDL_FreeSurface(backgroundImage);
         return 1;
     }
-<<<<<<< Updated upstream
-=======
     
     SDL_Surface *spelareImage = IMG_Load("resources/zombie.png");
     if(!spelareImage){
@@ -77,7 +60,6 @@ int main(int argv, char **args)
         printf("Error: %s\n",SDL_GetError());
         return 1;
     }
->>>>>>> Stashed changes
 
     SDL_Rect zombieRect[3]; // create 3 zombies
     for (int i = 0; i < 3; i++)
@@ -87,12 +69,9 @@ int main(int argv, char **args)
         zombieRect[i].w = zombieImage->w / 2; // make zombies half size
         zombieRect[i].h = zombieImage->h / 10;
     }
-<<<<<<< Updated upstream
-=======
 
     Spelare *spelare1 = createSpelare(WINDOW_WIDTH/2,WINDOW_HEIGHT/2, pRenderer,WINDOW_WIDTH,WINDOW_HEIGHT);
 
->>>>>>> Stashed changes
 
     int isRunning = 1;
     SDL_Event event;
@@ -111,33 +90,19 @@ int main(int argv, char **args)
 
         updateZombies(zombieRect, 3); // update the zombies' positions
 
-<<<<<<< Updated upstream
-        SDL_RenderClear(renderer);
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-
-        SDL_RenderCopy(renderer, backgroundTexture, NULL, NULL);
-=======
         SDL_RenderClear(pRenderer);
         SDL_SetRenderDrawColor(pRenderer, 0, 0, 0, 255);
 
         SDL_RenderCopy(pRenderer, backgroundTexture, NULL, NULL);
         SDL_RenderCopy(pRenderer,spelareTexture, NULL, NULL);
->>>>>>> Stashed changes
 
         // Render all zombies
         for (int i = 0; i < 3; i++)
         {
-<<<<<<< Updated upstream
-            SDL_RenderCopy(renderer, zombieTexture, NULL, &zombieRect[i]);
-        }
-        
-        SDL_RenderPresent(renderer);
-=======
             SDL_RenderCopy(pRenderer, zombieTexture, NULL, &zombieRect[i]);
         }
         
         SDL_RenderPresent(pRenderer);
->>>>>>> Stashed changes
 
         // Add delay to slow down the zombies' movement
         SDL_Delay(10); // add 10 millisecond delay
@@ -149,11 +114,7 @@ int main(int argv, char **args)
     SDL_FreeSurface(backgroundImage);
     SDL_DestroyTexture(zombieTexture);
     SDL_FreeSurface(zombieImage);
-<<<<<<< Updated upstream
-    SDL_DestroyRenderer(renderer);
-=======
     SDL_DestroyRenderer(pRenderer);
->>>>>>> Stashed changes
     SDL_DestroyWindow(window);
     SDL_Quit();
 
