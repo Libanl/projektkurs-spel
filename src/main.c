97b35fd347a85b884e88438a0de80be129ffc9ff
Johannes-Altaweel <129504364+Johannes-Altaweel@users.noreply.github.com>
2023-04-17 14:05:11 +0200

    Kodstruktur ändring
    ändrade kodstrukturern, minskade och uppdleade main i inti, run och closed funktioner

--- src/main.c
+++ src/main.c
@@ -10,9 +10,37 @@
 #define WINDOW_WIDTH 300
 #define WINDOW_HEIGHT 250
 
-int main(int argv, char **args)
-{
-    if (initMus() == -1)
+struct game{
+    SDL_Window *pWindow;
+    SDL_Renderer *pRenderer;
+    Spelare *pSpelare;
+    SDL_Rect zombieRect[5]; // create 3 zombies
+    SDL_Surface *pbackgroundImage;
+    SDL_Texture *pbackgroundTexture;
+    SDL_Surface *pZombieImage; 
+    SDL_Texture *pZombieTexture; 
+    SDL_Surface *pSpelareImage;
+    SDL_Texture *pSpelareTexture;
+};
+typedef struct game Game;
+
+int initiate(Game *pGame);
+void run(Game *pGame);
+void close(Game *pGame);
+
+int main(int argv, char** args){
+    Game g={0};
+    if(!initiate(&g)) return 1;
+    run(&g);
+    close(&g);
+
+    return 0;
+}
+
+
+int initiate(Game *pGame){
+
+     if (initMus() == -1)
     {
         printf("Kunde inte initiera ljudsystemet!\n");
         return 1;
@@ -20,69 +48,73 @@ int main(int argv, char **args)
 
     playMus("resources/spel.MP3");
 
-    SDL_Window *window = SDL_CreateWindow("Zombies COD", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
-    SDL_Renderer *pRenderer = SDL_CreateRenderer(window, -1, 0);
+    pGame->pWindow = SDL_CreateWindow("Zombies COD", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 800, 0);
+    pGame->pRenderer = SDL_CreateRenderer(pGame->pWindow, -1, 0);
 
-    SDL_Surface *backgroundImage = IMG_Load("resources/28256.jpg");
-    if (!backgroundImage)
+    pGame->pbackgroundImage = IMG_Load("resources/28256.jpg");
+    if (!pGame->pbackgroundImage)
     {
-        printf("Error! BackgroundImage failed\n", IMG_GetError());
+        printf("Error! backgroundImage failed\n", IMG_GetError());
         return 1;
     }
 
-    SDL_Texture *backgroundTexture = SDL_CreateTextureFromSurface(pRenderer, backgroundImage);
-    if (!backgroundTexture)
+    pGame->pbackgroundTexture = SDL_CreateTextureFromSurface(pGame->pRenderer, pGame->pbackgroundImage);
+    if (!pGame->pbackgroundTexture)
     {
         printf("Failed to create background texture: %s\n", SDL_GetError());
-        SDL_FreeSurface(backgroundImage);
+        SDL_FreeSurface(pGame->pbackgroundImage);
         return 1;
     }
 
-    SDL_Surface *zombieImage = IMG_Load("resources/zombie.png");
-    if (!zombieImage)
+    pGame->pZombieImage = IMG_Load("resources/zombie.png");
+    if (!pGame->pZombieImage)
     {
         printf("Error! ZombieImage failed\n", IMG_GetError());
-        SDL_DestroyTexture(backgroundTexture);
-        SDL_FreeSurface(backgroundImage);
+        SDL_DestroyTexture(pGame->pbackgroundTexture);
+        SDL_FreeSurface(pGame->pbackgroundImage);
         return 1;
     }
 
-    SDL_Texture *zombieTexture = SDL_CreateTextureFromSurface(pRenderer, zombieImage);
-    if (!zombieTexture)
+    pGame->pZombieTexture = SDL_CreateTextureFromSurface(pGame->pRenderer, pGame->pZombieImage);
+    if (!pGame->pZombieTexture)
     {
         printf("Failed to create zombie texture: %s\n", SDL_GetError());
-        SDL_FreeSurface(zombieImage);
-        SDL_DestroyTexture(backgroundTexture);
-        SDL_FreeSurface(backgroundImage);
+        SDL_FreeSurface(pGame->pZombieImage);
+        SDL_DestroyTexture(pGame->pbackgroundTexture);
+        SDL_FreeSurface(pGame->pbackgroundImage);
         return 1;
     }
 
-    SDL_Surface *spelareImage = IMG_Load("resources/soldier2.png");
-    if (!spelareImage)
+    pGame->pSpelareImage = IMG_Load("resources/soldier2.png");
+    if (!pGame->pSpelareImage)
     {
         printf("Error: %s\n", SDL_GetError());
         return 1;
     }
 
-    SDL_Texture *spelareTexture = SDL_CreateTextureFromSurface(pRenderer, spelareImage);
-    if (!spelareTexture)
+    pGame->pSpelareTexture = SDL_CreateTextureFromSurface(pGame->pRenderer, pGame->pSpelareImage);
+    if (!pGame->pSpelareTexture)
     {
         printf("Error: %s\n", SDL_GetError());
         return 1;
     }
-    SDL_Rect spelareRect = {WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, spelareImage->w / 2, spelareImage->h / 6};
 
-    SDL_Rect zombieRect[5]; // create 3 zombies
+     // create 3 zombies
     for (int i = 0; i < 5; i++)
     {
-        zombieRect[i].x = 100 + i * 200;
-        zombieRect[i].y = 300;
-        zombieRect[i].w = zombieImage->w / 2; // make zombies half size
-        zombieRect[i].h = zombieImage->h / 10;
+        pGame->zombieRect[i].x = 100 + i * 200;
+        pGame->zombieRect[i].y = 300;
+        pGame->zombieRect[i].w = pGame->pZombieImage->w / 2; // make zombies half size
+        pGame->zombieRect[i].h = pGame->pZombieImage->h / 10;
     }
 
-    Spelare *spelare1 = createSpelare(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, pRenderer, WINDOW_WIDTH, WINDOW_HEIGHT);
+    Spelare *spelare1 = createSpelare(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, pGame->pRenderer, WINDOW_WIDTH, WINDOW_HEIGHT);
+}
 
+
+void run(Game *pGame){
+    
+    SDL_Rect spelareRect = {WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, pGame->pSpelareImage->w / 7, pGame->pSpelareImage->h / 16};
     int isRunning = 1;
     SDL_Event event;
 
@@ -98,37 +130,38 @@ int main(int argv, char **args)
             }
         }
 
-        updateZombies(zombieRect, 5); // update the zombies' positions
+        updateZombies(pGame->zombieRect, 5); // update the zombies' positions
 
-        SDL_RenderClear(pRenderer);
-        SDL_SetRenderDrawColor(pRenderer, 0, 0, 0, 255);
+        SDL_RenderClear(pGame->pRenderer);
+        SDL_SetRenderDrawColor(pGame->pRenderer, 0, 0, 0, 255);
 
-        SDL_RenderCopy(pRenderer, backgroundTexture, NULL, NULL);
-        SDL_RenderCopy(pRenderer, spelareTexture, NULL, &spelareRect);
+        SDL_RenderCopy(pGame->pRenderer, pGame->pbackgroundTexture, NULL, NULL);
+        SDL_RenderCopy(pGame->pRenderer, pGame->pSpelareTexture, NULL, &spelareRect);
 
         // Render all zombies
         for (int i = 0; i < 5; i++)
         {
-            SDL_RenderCopy(pRenderer, zombieTexture, NULL, &zombieRect[i]);
+            SDL_RenderCopy(pGame->pRenderer, pGame->pZombieTexture, NULL, &pGame->zombieRect[i]);
         }
 
-        SDL_RenderPresent(pRenderer);
+        SDL_RenderPresent(pGame->pRenderer);
 
         // Add delay to slow down the zombies' movement
         SDL_Delay(10); // add 10 millisecond delay
     }
-
-    stopMus();
-    cleanMu();
-    SDL_DestroyTexture(spelareTexture);
-    SDL_FreeSurface(spelareImage);
-    SDL_DestroyTexture(backgroundTexture);
-    SDL_FreeSurface(backgroundImage);
-    SDL_DestroyTexture(zombieTexture);
-    SDL_FreeSurface(zombieImage);
-    SDL_DestroyRenderer(pRenderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
-    return 0;
 }
+
+    void close(Game *pGame){
+        stopMus();
+        cleanMu();
+        SDL_DestroyTexture(pGame->pSpelareTexture);
+        SDL_FreeSurface(pGame->pSpelareImage);
+        SDL_DestroyTexture(pGame->pbackgroundTexture);
+        SDL_FreeSurface(pGame->pbackgroundImage);
+        SDL_DestroyTexture(pGame->pZombieTexture);
+        SDL_FreeSurface(pGame->pZombieImage);
+        SDL_DestroyRenderer(pGame->pRenderer);
+        SDL_DestroyWindow(pGame->pWindow);
+        SDL_Quit();
+
+    }
