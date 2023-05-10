d08891263d385bbc968217bd2eea8146bebb2c00
Libanl <129501428+Libanl@users.noreply.github.com>
2023-05-10 15:51:09 +0200

    Squashed commit of the following:
    commit fb16f0ebcb2ca87f2896b2d31627aca97eb5b36c
    Author: Libanl <129501428+Libanl@users.noreply.github.com>
    Date:   Wed May 10 15:43:25 2023 +0200
    
        Update main.c
    
    commit 0142fc32e417f86c5f4b12983a12278c131050a3
    Author: Libanl <129501428+Libanl@users.noreply.github.com>
    Date:   Tue May 9 23:58:33 2023 +0200
    
        Tried adding colission detection with bullets dosent work
    
    commit 338c7f24eb8466fffb48742566568ae50f3d51c2
    Author: Libanl <129501428+Libanl@users.noreply.github.com>
    Date:   Tue May 9 23:29:02 2023 +0200
    
        Added function to clear zombies after game restarts
    
    commit ff9c93b952655a272488ce064ee0a31519aadff6
    Author: Libanl <129501428+Libanl@users.noreply.github.com>
    Date:   Tue May 9 18:02:10 2023 +0200
    
        Chnaged player sprite size
    
        Problem with collision, player dosent collide correctly with zombie at certain angles
    
    commit 984354e6c1a0a4e50aa4c5f1d0d4f0a629505352
    Author: Libanl <129501428+Libanl@users.noreply.github.com>
    Date:   Tue May 9 17:54:35 2023 +0200
    
        Added collision detection
    
    commit 868ece78e96c1d03f4a7b8b4663019d485a976b1
    Author: Libanl <129501428+Libanl@users.noreply.github.com>
    Date:   Tue May 9 16:29:48 2023 +0200
    
        Music can now be muted
    
    commit 389a023b411717647f3b985816146aae9cb64ae1
    Author: Libanl <129501428+Libanl@users.noreply.github.com>
    Date:   Mon May 8 22:51:11 2023 +0200
    
        Small fixes and max fps set to 30
    
    commit 940d9d28f257e673beabcde105f19aa28f3f1322
    Author: Libanl <129501428+Libanl@users.noreply.github.com>
    Date:   Mon May 8 21:44:39 2023 +0200
    
        Zombies cant go off screen

--- src/main.c
+++ src/main.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <stdbool.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
-#include "../includes/zombie.h" // include the zombies header file
+#include "../includes/zombie.h" // include the zombies header file                   //game over funkar konstigt det blir seg.fault efter //collision detection funkar men inte riktigt som den ska
 #include "../includes/spelare.h"
-#include "../includes/music.h"
+#include "../includes/music.h"                                                       //bullet collision behövs det en rect member i structen?
 #include "../includes/bullet.h"
 #include "../includes/text.h"
 #define WINDOW_WIDTH 1000
@@ -31,15 +32,15 @@ struct game
     Bullet *pBullet;
     SDL_Rect zombieRect[MAX_ZOMBIES];
     ZombieImage *pZombieImage;
-    Zombie *pZombies[100];
+    Zombie *pZombies[MAX_ZOMBIES];
     SDL_Surface *pGame_StartBackgroundimage;
     SDL_Texture *pGame_StartbackgroundTexture;
     SDL_Surface *pbackgroundImage;
     SDL_Texture *pbackgroundTexture;
     int Nrofzombies;
     int timeForNextZombie;
-    //SDL_Surface *pZombieImage; 
-   // SDL_Texture *pZombieTexture;
+    // SDL_Surface *pZombieImage;
+    // SDL_Texture *pZombieTexture;
     int MoveUp;
     int MoveLeft;
     int MoveDown;
@@ -47,7 +48,6 @@ struct game
     int mouseState;
     TTF_Font *pScoreFont, *pFont, *pOverFont;
     Text *pScoreText, *pOverText, *pStartText;
-    ;
     int gameTimeM;
     int startTime; // in ms
     int gameTime;  // in s
@@ -63,8 +63,7 @@ int getTime(Game *pGame);
 int getMilli(Game *pGame);
 void updateGameTime(Game *pGame);
 void updateNrOfZombies(Game *pGame);
-
-//void CheckCollison( Game *pGame, int zombieCount);
+void resetZombies(Game *pGame);
 
 int main(int argv, char **args)
 {
@@ -159,8 +158,8 @@ int initiate(Game *pGame)
     pGame->MoveRight = 0;
 
     pGame->pSpelare = createSpelare(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, pGame->pRenderer, WINDOW_WIDTH, WINDOW_HEIGHT);
-    //pGame->pZombie =  createZombie(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, pGame->pRenderer, WINDOW_WIDTH, WINDOW_HEIGHT);
-     pGame->pZombieImage = initiateZombie(pGame->pRenderer);
+    // pGame->pZombie =  createZombie(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, pGame->pRenderer, WINDOW_WIDTH, WINDOW_HEIGHT);
+    pGame->pZombieImage = initiateZombie(pGame->pRenderer);
 
     pGame->pFont = TTF_OpenFont("resources/arial.ttf", 100);
     pGame->pScoreFont = TTF_OpenFont("resources/arial.ttf", 70);
@@ -172,12 +171,12 @@ int initiate(Game *pGame)
         return 0;
     }
 
-    pGame->Nrofzombies=0;
-    pGame->timeForNextZombie=3;
-   
+    pGame->Nrofzombies = 0;
+    resetZombies(pGame);
+    pGame->timeForNextZombie = 3;
 
-    pGame->pOverText = createText(pGame->pRenderer,238,168,65,pGame->pFont,"Game over",WINDOW_WIDTH/2,WINDOW_HEIGHT/2);
-    pGame->pStartText = createText(pGame->pRenderer,238,168,65,pGame->pOverFont,"Press space to start OR M (TO GO BACK TO MENU)",WINDOW_WIDTH/2,WINDOW_HEIGHT/2+100);
+    pGame->pOverText = createText(pGame->pRenderer, 238, 168, 65, pGame->pFont, "Game over", WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2);
+    pGame->pStartText = createText(pGame->pRenderer, 238, 168, 65, pGame->pOverFont, "Press space to start OR M (TO GO BACK TO MENU)", WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 + 100);
     pGame->startTime = SDL_GetTicks64();
     pGame->gameTime = -1;
     pGame->state = START;
@@ -189,73 +188,106 @@ void run(Game *pGame)
 
     int keys[SDL_NUM_SCANCODES] = {0}; // Initialize an array to store key states
     int isRunning = 1;
-    int first=1;
-    int pressed=0;
-    int j=0;
+    int first = 1;
+    int pressed = 0;
+    int j = 0;
     SDL_Event event;
-    int zombieCount = 0;      // Keep track of the current number of zombies
-    Uint32 lastSpawnTime = 0; // Keep track of the time since the last zombie spawn
+    int zombieCount = 0;                  // Keep track of the current number of zombies
+    Uint32 lastSpawnTime = 0;             // Keep track of the time since the last zombie spawn
+    Uint32 startTime, endTime, frameTime; // decide fps/frames per second
+    frameTime = 1000 / 30;
     while (isRunning)
     {
         switch (pGame->state)
         {
-            case ONGOING:
-                while (SDL_PollEvent(&event)){   
-                    switch (event.type){
-                    case SDL_QUIT:
+        case ONGOING:
+            while (SDL_PollEvent(&event))
+            {
+                switch (event.type)
+                {
+                case SDL_QUIT:
+                    isRunning = 0;
+                    break;
+                case SDL_KEYDOWN:
+                    if (event.key.keysym.sym == SDLK_ESCAPE)
+                    {
                         isRunning = 0;
-                        break;
-                    case SDL_KEYDOWN:
-                        if (event.key.keysym.sym == SDLK_ESCAPE)
-                        {
-                            isRunning = 0;
-                        }
-                    default: handleInput(&event,pGame,keys);
-                        break;
                     }
+                    else
+                    {
+                        switch (event.key.keysym.scancode)
+                        {
+                        case SDL_SCANCODE_P:
+                            if (event.key.repeat == 0)
+                            {
+                                stopMus();
+                                cleanMu();
+                            }
+                            break;
+                        case SDL_SCANCODE_O:
+                            if (event.key.repeat == 0)
+                            {
+                                playMus("resources/spel.MP3");
+                            }
+                            break;
+                        default:
+                            handleInput(&event, pGame, keys);
+                            break;
+                        }
+                    }
+                    break;
+                default:
+                    handleInput(&event, pGame, keys);
+                    break;
                 }
+            }
 
-                Uint32 currentTime = SDL_GetTicks();
-                updateNrOfZombies(pGame);
-                updateSpelare(pGame->pSpelare);
-                
-                
-                //updateZombies(pGame->zombieRect, zombieCount); // update the zombies' positions
-                updateGameTime(pGame);
-                SDL_RenderClear(pGame->pRenderer);        
-                SDL_RenderCopy(pGame->pRenderer, pGame->pbackgroundTexture, NULL, NULL);
-                for(int i=0;i<(pGame->Nrofzombies);i++) 
-                {
-                    updateZombie(pGame->pZombies[i]);
-                }
-                drawSpelare(pGame->pSpelare);
-                for(int i=0;i<pGame->Nrofzombies;i++)
-                {
-                    drawZombie(pGame->pZombies[i]);
-                }
-                //drawZombies(pGame->pZombie);
-                // Render all zombies
-                /*if(aliveBullet(pGame->pBullet)){
-                    CheckCollison(pGame, zombieCount);
-                }*/
+            Uint32 currentTime = SDL_GetTicks();
+            startTime = SDL_GetTicks();
+            SDL_RenderClear(pGame->pRenderer);
+            updateNrOfZombies(pGame);
+            updateSpelare(pGame->pSpelare);
+            updateGameTime(pGame);
+            SDL_RenderCopy(pGame->pRenderer, pGame->pbackgroundTexture, NULL, NULL);
+            for (int i = 0; i < (pGame->Nrofzombies); i++)
+            {
+                updateZombie(pGame->pZombies[i]);
+            }
+            drawSpelare(pGame->pSpelare);
+            for (int i = 0; i < pGame->Nrofzombies; i++)
+            {
+                drawZombie(pGame->pZombies[i]);
+            }
+            for (int i = 0; i < pGame->Nrofzombies; i++)
+            {
+                if (collideSpelare(pGame->pSpelare, getRectZombie(pGame->pZombies[i])))
+                    pGame->state = GAME_OVER;
+            }
+            if (pGame->pScoreText)
+            {
+                drawText(pGame->pScoreText);
+            }
 
-                if(pGame->pScoreText) 
-                {
-                    drawText(pGame->pScoreText);
-                }
-                SDL_Delay(10);
+            SDL_RenderPresent(pGame->pRenderer);
+            if (getTime(pGame) == 100)
+            {
+                pGame->state = GAME_OVER;
+            }
 
-                SDL_RenderPresent(pGame->pRenderer);
-                if(getTime(pGame)==100){
-                    pGame->state= GAME_OVER;
-                }
-                break;
-                case GAME_OVER:
-                drawText(pGame->pOverText);
-                drawText(pGame->pStartText);
-                resetSpelare(pGame->pSpelare);
-                pGame->Nrofzombies=0;
-                pressed=0;  
+            endTime = SDL_GetTicks();
+            SDL_Delay(10);
+            /*if (endTime - startTime < frameTime)
+            {
+                SDL_Delay(frameTime - (endTime - startTime));
+            }*/
+
+            break;
+        case GAME_OVER:
+            drawText(pGame->pOverText);
+            drawText(pGame->pStartText);
+            resetSpelare(pGame->pSpelare);
+            pGame->Nrofzombies = 0;
+            pressed = 0;
         case START:
             if (first == 1)
             {
@@ -313,8 +345,6 @@ void handleInput(SDL_Event *pEvent, Game *pGame, int keys[])
     {
         keys[pEvent->key.keysym.scancode] = 0; // Set the corresponding key state to false
     }
-
-    // Check for combination of keys pressed together
 
     if (keys[SDL_SCANCODE_W] && keys[SDL_SCANCODE_A])
     {
@@ -382,8 +412,8 @@ void close(Game *pGame)
     SDL_FreeSurface(pGame->pGame_StartBackgroundimage);
     SDL_DestroyTexture(pGame->pbackgroundTexture);
     SDL_FreeSurface(pGame->pbackgroundImage);
-    //SDL_DestroyTexture(pGame->pZombieTexture);
-    //SDL_FreeSurface(pGame->pZombieImage);
+    // SDL_DestroyTexture(pGame->pZombieTexture);
+    // SDL_FreeSurface(pGame->pZombieImage);
     SDL_DestroyRenderer(pGame->pRenderer);
     SDL_DestroyWindow(pGame->pWindow);
     if (pGame->pScoreText)
@@ -425,26 +455,39 @@ void updateGameTime(Game *pGame)
     }
 }
 
-
-void updateNrOfZombies(Game *pGame){
-    if(getTime(pGame)>pGame->timeForNextZombie&& pGame->Nrofzombies<100){
-        (pGame->timeForNextZombie)=(pGame->timeForNextZombie)+1;//seconds till next asteroid
-        pGame->pZombies[pGame->Nrofzombies] = createZombie(pGame->pZombieImage,WINDOW_WIDTH,WINDOW_HEIGHT);
-        pGame->Nrofzombies++; 
-    }    
-     
+void updateNrOfZombies(Game *pGame)
+{
+    if (getTime(pGame) > pGame->timeForNextZombie && pGame->Nrofzombies < MAX_ZOMBIES)
+    {
+        (pGame->timeForNextZombie) = (pGame->timeForNextZombie) + 1; // seconds till next asteroid
+        pGame->pZombies[pGame->Nrofzombies] = createZombie(pGame->pZombieImage, WINDOW_WIDTH, WINDOW_HEIGHT);
+        pGame->Nrofzombies++;
+    }
 }
 
-
+void resetZombies(Game *pGame)
+{
+    for (int i = 0; i < pGame->Nrofzombies; i++)
+    {
+        destroyZombie(pGame->pZombies[i]);
+    }
+    pGame->Nrofzombies = 0;
+}
 
 /*void CheckCollison(Game *pGame, int zombieCount)
 {
     int i;
-    for(i = 0; i < zombieCount; i++){
-        if((xBullet(pGame->pBullet) > pGame->zombieRect[i].x && xBullet(pGame->pBullet) < pGame->zombieRect[i].x + 30) &&
-           (yBullet(pGame->pBullet) > pGame->zombieRect[i].y && yBullet(pGame->pBullet) < pGame->zombieRect[i].y + 30)){
+    for (i = 0; i < zombieCount; i++)
+    {
+        if ((xBullet(pGame->pBullet) > pGame->zombieRect[i].x && xBullet(pGame->pBullet) < pGame->zombieRect[i].x + 30) &&
+            (yBullet(pGame->pBullet) > pGame->zombieRect[i].y && yBullet(pGame->pBullet) < pGame->zombieRect[i].y + 30))
+        {
             printf("test\n");
+
             break;
         }
     }
-}*/
+
+
+}
+*/
--- src/spelare.c
+++ src/spelare.c
@@ -17,123 +17,125 @@ struct spelare
     SDL_Rect shipRect[16];
     SDL_Rect gsprites[16];
     SDL_RendererFlip flip;
-
 };
 
-    Spelare *createSpelare(int x, int y, SDL_Renderer *pRenderer, int window_width, int window_height){
+Spelare *createSpelare(int x, int y, SDL_Renderer *pRenderer, int window_width, int window_height)
+{
     Spelare *pSpelare = malloc(sizeof(struct spelare));
-    pSpelare->vx=pSpelare->vy=0;
-    pSpelare->angle=0;
-    pSpelare->frame=6;
-    pSpelare->nyframe=6;
+    pSpelare->vx = pSpelare->vy = 0;
+    pSpelare->angle = 0;
+    pSpelare->frame = 6;
+    pSpelare->nyframe = 6;
     pSpelare->pBullet = createBullet(pRenderer, window_width, window_height);
     pSpelare->window_width = window_width;
     pSpelare->window_height = window_height;
     pSpelare->flip = SDL_FLIP_NONE;
     pSpelare->pRenderer = pRenderer;
     SDL_Surface *pSurface = IMG_Load("resources/soldiertest.PNG");
-    if(!pSurface){
-        printf("Error: %s\n",SDL_GetError());
+    if (!pSurface)
+    {
+        printf("Error: %s\n", SDL_GetError());
         return NULL;
     }
-    pSpelare->pTexture= SDL_CreateTextureFromSurface(pRenderer, pSurface);
-    if(!pSpelare->pTexture){
-        printf("Error: %s\n",SDL_GetError());
+    pSpelare->pTexture = SDL_CreateTextureFromSurface(pRenderer, pSurface);
+    if (!pSpelare->pTexture)
+    {
+        printf("Error: %s\n", SDL_GetError());
         return NULL;
     }
     SDL_FreeSurface(pSurface);
-    SDL_QueryTexture(pSpelare->pTexture,NULL,NULL,&(pSpelare->shipRect[pSpelare->frame].w),&(pSpelare->shipRect[pSpelare->frame].h));
-    pSpelare->gsprites[ 0 ].x =0;
-    pSpelare->gsprites[ 0 ].y =0;
-    pSpelare->gsprites[ 0 ].w =96;
-    pSpelare->gsprites[ 0 ].h =70;
-    
-    pSpelare->gsprites[ 1 ].x =288;
-    pSpelare->gsprites[ 1 ].y =0;
-    pSpelare->gsprites[ 1 ].w =96;
-    pSpelare->gsprites[ 1 ].h =70;
-    
-    pSpelare->gsprites[ 2 ].x =288;
-    pSpelare->gsprites[ 2 ].y =140;
-    pSpelare->gsprites[ 2 ].w =96;
-    pSpelare->gsprites[ 2 ].h =70;
-    
-    pSpelare->gsprites[ 3 ].x =480;
-    pSpelare->gsprites[ 3 ].y =140;
-    pSpelare->gsprites[ 3 ].w =96;
-    pSpelare->gsprites[ 3 ].h =70;
-    
-    pSpelare->gsprites[ 4 ].x =0;
-    pSpelare->gsprites[ 4 ].y =210;
-    pSpelare->gsprites[ 4 ].w =96;
-    pSpelare->gsprites[ 4 ].h =70;
-    
-    pSpelare->gsprites[ 5 ].x =288;
-    pSpelare->gsprites[ 5 ].y =210;
-    pSpelare->gsprites[ 5 ].w =96;
-    pSpelare->gsprites[ 5 ].h =70;
-    
-    pSpelare->gsprites[ 6 ].x =480;
-    pSpelare->gsprites[ 6 ].y =0;
-    pSpelare->gsprites[ 6 ].w =96;
-    pSpelare->gsprites[ 6 ].h =70;
+    SDL_QueryTexture(pSpelare->pTexture, NULL, NULL, &(pSpelare->shipRect[pSpelare->frame].w), &(pSpelare->shipRect[pSpelare->frame].h));
+    pSpelare->gsprites[0].x = 0;
+    pSpelare->gsprites[0].y = 0;
+    pSpelare->gsprites[0].w = 96;
+    pSpelare->gsprites[0].h = 70;
 
-    for(int i=0;i<7;i++)
+    pSpelare->gsprites[1].x = 288;
+    pSpelare->gsprites[1].y = 0;
+    pSpelare->gsprites[1].w = 96;
+    pSpelare->gsprites[1].h = 70;
+
+    pSpelare->gsprites[2].x = 288;
+    pSpelare->gsprites[2].y = 140;
+    pSpelare->gsprites[2].w = 96;
+    pSpelare->gsprites[2].h = 70;
+
+    pSpelare->gsprites[3].x = 480;
+    pSpelare->gsprites[3].y = 140;
+    pSpelare->gsprites[3].w = 96;
+    pSpelare->gsprites[3].h = 70;
+
+    pSpelare->gsprites[4].x = 0;
+    pSpelare->gsprites[4].y = 210;
+    pSpelare->gsprites[4].w = 96;
+    pSpelare->gsprites[4].h = 70;
+
+    pSpelare->gsprites[5].x = 288;
+    pSpelare->gsprites[5].y = 210;
+    pSpelare->gsprites[5].w = 96;
+    pSpelare->gsprites[5].h = 70;
+
+    pSpelare->gsprites[6].x = 480;
+    pSpelare->gsprites[6].y = 0;
+    pSpelare->gsprites[6].w = 96;
+    pSpelare->gsprites[6].h = 70;
+
+    for (int i = 0; i < 7; i++)
     {
-        pSpelare->shipRect[i]=pSpelare->gsprites[i];
-        pSpelare->shipRect[i].w=pSpelare->shipRect[i].w/1.5;
-        pSpelare->shipRect[i].h=pSpelare->shipRect[i].h/1.5;
-        pSpelare->x=x-pSpelare->shipRect[i].w;
-        pSpelare->y=y-pSpelare->shipRect[i].h;
+        pSpelare->shipRect[i] = pSpelare->gsprites[i];
+        pSpelare->shipRect[i].w = pSpelare->shipRect[i].w / 1.5;
+        pSpelare->shipRect[i].h = pSpelare->shipRect[i].h / 1.5;
+        pSpelare->x = x - pSpelare->shipRect[i].w;
+        pSpelare->y = y - pSpelare->shipRect[i].h;
     }
     return pSpelare;
-    
 }
 
-void moveLeft(Spelare *pSpelare){
-   
-    if (pSpelare->shipRect[pSpelare->frame].x>0)
+void moveLeft(Spelare *pSpelare)
+{
+    if (pSpelare->shipRect[pSpelare->frame].x > 0)
     {
-        pSpelare->x-=5;
+        pSpelare->x -= 5;
+        pSpelare->shipRect[pSpelare->frame].x = pSpelare->x;
     }
     pSpelare->flip = SDL_FLIP_HORIZONTAL;
-    if(pSpelare->frame == 2)
+    if (pSpelare->frame == 2)
     {
         pSpelare->nyframe = 3;
     }
     else
     {
-         pSpelare->nyframe = 2;
+        pSpelare->nyframe = 2;
     }
-
 }
 
-void moveRight(Spelare *pSpelare){
-    
-    if (pSpelare->shipRect[pSpelare->frame].x<970)
+void moveRight(Spelare *pSpelare)
+{
+    if (pSpelare->shipRect[pSpelare->frame].x < 970)
     {
-        pSpelare->x+=5;
+        pSpelare->x += 5;
+        pSpelare->shipRect[pSpelare->frame].x = pSpelare->x;
     }
     pSpelare->flip = SDL_FLIP_NONE;
-    if(pSpelare->frame == 2)
+    if (pSpelare->frame == 2)
     {
-         pSpelare->nyframe = 3;
+        pSpelare->nyframe = 3;
     }
     else
     {
         pSpelare->nyframe = 2;
     }
-
 }
 
-void moveUp(Spelare *pSpelare){
+void moveUp(Spelare *pSpelare)
+{
 
-    if (pSpelare->shipRect[pSpelare->frame].y>0)
+    if (pSpelare->y > 0)
     {
-        pSpelare->y-=5;
+        pSpelare->y -= 5;
     }
     pSpelare->flip = SDL_FLIP_NONE;
-    if(pSpelare->frame == 4)
+    if (pSpelare->frame == 4)
     {
         pSpelare->nyframe = 5;
     }
@@ -141,16 +143,18 @@ void moveUp(Spelare *pSpelare){
     {
         pSpelare->nyframe = 4;
     }
+    pSpelare->shipRect[pSpelare->frame].y = pSpelare->y;
 }
 
-void moveDown(Spelare *pSpelare){
+void moveDown(Spelare *pSpelare)
+{
 
-    if (pSpelare->shipRect[pSpelare->frame].y<600)
+    if (pSpelare->y < 600)
     {
-        pSpelare->y+=5;
+        pSpelare->y += 5;
     }
     pSpelare->flip = SDL_FLIP_NONE;
-    if(pSpelare->frame == 0)
+    if (pSpelare->frame == 0)
     {
         pSpelare->nyframe = 1;
     }
@@ -158,13 +162,14 @@ void moveDown(Spelare *pSpelare){
     {
         pSpelare->nyframe = 0;
     }
+    pSpelare->shipRect[pSpelare->frame].y = pSpelare->y;
 }
 
-
-void updateSpelare(Spelare *pSpelare){
-    pSpelare->shipRect[pSpelare->frame].x=pSpelare->x;
-    pSpelare->shipRect[pSpelare->frame].y=pSpelare->y;
-    pSpelare->frame=pSpelare->nyframe;
+void updateSpelare(Spelare *pSpelare)
+{
+    pSpelare->shipRect[pSpelare->frame].x = pSpelare->x;
+    pSpelare->shipRect[pSpelare->frame].y = pSpelare->y;
+    pSpelare->frame = pSpelare->nyframe;
     updateBullet(pSpelare->pBullet);
 }
 
@@ -179,14 +184,12 @@ void fireSpelare(Spelare *pSpelare, int m1, int m2, int m3, int m4)
     startBullet(pSpelare->pBullet, pSpelare->x + pSpelare->shipRect[pSpelare->frame].w / 2, pSpelare->y + pSpelare->shipRect[pSpelare->frame].h / 2, moveUp, moveLeft, moveDown, moveRight);
 }
 
-
 void drawSpelare(Spelare *pSpelare)
 {
     if (aliveBullet(pSpelare->pBullet))
         drawBullet(pSpelare->pBullet, pSpelare->pRenderer);
-    SDL_RenderCopyEx(pSpelare->pRenderer,pSpelare->pTexture,&(pSpelare->gsprites[pSpelare->frame]),&(pSpelare->shipRect[pSpelare->frame]),pSpelare->angle,NULL,pSpelare->flip);
+    SDL_RenderCopyEx(pSpelare->pRenderer, pSpelare->pTexture, &(pSpelare->gsprites[pSpelare->frame]), &(pSpelare->shipRect[pSpelare->frame]), pSpelare->angle, NULL, pSpelare->flip);
 }
-
 
 void destroySpelare(Spelare *pSpelare)
 {
@@ -194,10 +197,26 @@ void destroySpelare(Spelare *pSpelare)
     free(pSpelare);
 }
 
-void resetSpelare(Spelare *pSpelare){
-    pSpelare->x=pSpelare->window_width/2;
-    pSpelare->y=pSpelare->window_height/2;
-    pSpelare->angle=0;
-    pSpelare->vx=pSpelare->vy=0;
-    pSpelare->nyframe=6;
+void resetSpelare(Spelare *pSpelare)
+{
+    pSpelare->x = pSpelare->window_width / 2;
+    pSpelare->y = pSpelare->window_height / 2;
+    pSpelare->angle = 0;
+    pSpelare->vx = pSpelare->vy = 0;
+    pSpelare->nyframe = 6;
+}
+
+/*int collideSpelare(Spelare *pSpelare, SDL_Rect rect)
+{
+    return distance(pSpelare->shipRect.x + pSpelare->shipRect.w / 2, pSpelare->shipRect.y + pSpelare->shipRect.h / 2, rect.x + rect.w / 2, rect.y + rect.h / 2) < (pSpelare->shipRect.w + rect.w) / 2;
+}*/
+
+int collideSpelare(Spelare *pSpelare, SDL_Rect rect)
+{
+    return distance(pSpelare->shipRect->x + pSpelare->shipRect->w / 2, pSpelare->shipRect->y + pSpelare->shipRect->h / 2, rect.x + rect.w / 2, rect.y + rect.h / 2) < (pSpelare->shipRect->w + rect.w) / 2;
+}
+
+float distance(int x1, int y1, int x2, int y2)
+{
+    return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 }
