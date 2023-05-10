cf7109f7000f763e3f70203abea4f26ff4dc3f5e
robel145 <129504103+robel145@users.noreply.github.com>
2023-05-10 14:53:55 +0200

    Revert "tetsBullet"
    This reverts commit a2334255280777d130252594b91717c78b16c84e.

--- includes/bullet.h
+++ includes/bullet.h
@@ -11,10 +11,9 @@ void updateBullet(Bullet *pBullet);
 void startBullet(Bullet *pBullet, float x, float y, int moveup, int moveleft, int movedown, int moveright);
 void killBullet(Bullet *pBullet);
 void drawBullet(Bullet *pBullet,SDL_Renderer *pRenderer);
-int xBullet(Bullet *pBullet);
-int yBullet(Bullet *pBullet);
+float xBullet(Bullet *pBullet);
+float yBullet(Bullet *pBullet);
 void destroyBullet(Bullet *pBullet);
 int aliveBullet(Bullet *pBullet);
-SDL_Rect getRectBullet(Bullet *pBullet);
 
 #endif
